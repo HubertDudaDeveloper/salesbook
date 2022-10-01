448fd1efaa48b19ec86c1b7b5fdbfbf3ce2ea8f8
HubertDudaDeveloper <hubertdudadeveloper@gmail.com>
2022-10-01 16:21:17 +0200

    column one :rocket:
    Added column, API etc.

--- dist/style.493e6e98.js
+++ dist/style.493e6e98.js
@@ -117,61 +117,79 @@ parcelRequire = (function (modules, cache, entry, globalName) {
   }
 
   return newRequire;
-})({"main.js":[function(require,module,exports) {
-'use strict';
+})({"../node_modules/parcel-bundler/src/builtins/bundle-url.js":[function(require,module,exports) {
+var bundleURL = null;
 
-function getToday() {
-  var today = new Date();
-  var dd = String(today.getDate()).padStart(2, '0');
-  var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-
-  var yyyy = today.getFullYear();
-  today = yyyy + '-' + mm + '-' + dd;
-  return today;
-}
-
-function getRequest() {
-  var xhr = new XMLHttpRequest();
-  xhr.open('GET', "https://api.apilayer.com/exchangerates_data/timeseries?start_date=2022-09-24&end_date=2022-10-01&base=PLN", true);
-  xhr.setRequestHeader('apikey', '');
-
-  xhr.onload = function () {
-    if (xhr.status === 200) {
-      window.localStorage.setItem('data', this.responseText);
-    } else {
-      console.log('Error: ' + xhr.status);
-    }
-  };
-
-  xhr.send(null);
-}
-
-function test() {
-  var item = window.localStorage.getItem('data');
-
-  for (var k in JSON.parse(item).rates[getToday()]) {
-    console.log(k);
-    var el = document.getElementById('rowList');
-    var node = document.createElement('button');
-    var valnode = document.createTextNode(' ' + JSON.parse(item).rates[getToday()][k]);
-    var textnode = document.createTextNode(k);
-    node.appendChild(textnode);
-    node.appendChild(valnode);
-    el.appendChild(node);
-    console.log(valnode);
+function getBundleURLCached() {
+  if (!bundleURL) {
+    bundleURL = getBundleURL();
   }
 
-  console.log(getToday());
+  return bundleURL;
 }
 
-if (localStorage.data === undefined) {
-  getRequest();
-  test();
-} else {
-  console.log('data w localstorage');
-  test();
+function getBundleURL() {
+  // Attempt to find the URL of the current script and use that as the base URL
+  try {
+    throw new Error();
+  } catch (err) {
+    var matches = ('' + err.stack).match(/(https?|file|ftp|chrome-extension|moz-extension):\/\/[^)\n]+/g);
+
+    if (matches) {
+      return getBaseURL(matches[0]);
+    }
+  }
+
+  return '/';
 }
-},{}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+
+function getBaseURL(url) {
+  return ('' + url).replace(/^((?:https?|file|ftp|chrome-extension|moz-extension):\/\/.+)?\/[^/]+(?:\?.*)?$/, '$1') + '/';
+}
+
+exports.getBundleURL = getBundleURLCached;
+exports.getBaseURL = getBaseURL;
+},{}],"../node_modules/parcel-bundler/src/builtins/css-loader.js":[function(require,module,exports) {
+var bundle = require('./bundle-url');
+
+function updateLink(link) {
+  var newLink = link.cloneNode();
+
+  newLink.onload = function () {
+    link.remove();
+  };
+
+  newLink.href = link.href.split('?')[0] + '?' + Date.now();
+  link.parentNode.insertBefore(newLink, link.nextSibling);
+}
+
+var cssTimeout = null;
+
+function reloadCSS() {
+  if (cssTimeout) {
+    return;
+  }
+
+  cssTimeout = setTimeout(function () {
+    var links = document.querySelectorAll('link[rel="stylesheet"]');
+
+    for (var i = 0; i < links.length; i++) {
+      if (bundle.getBaseURL(links[i].href) === bundle.getBundleURL()) {
+        updateLink(links[i]);
+      }
+    }
+
+    cssTimeout = null;
+  }, 50);
+}
+
+module.exports = reloadCSS;
+},{"./bundle-url":"../node_modules/parcel-bundler/src/builtins/bundle-url.js"}],"style/style.css":[function(require,module,exports) {
+var reloadCSS = require('_css_loader');
+
+module.hot.dispose(reloadCSS);
+module.hot.accept(reloadCSS);
+},{"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -375,5 +393,5 @@ function hmrAcceptRun(bundle, id) {
     return true;
   }
 }
-},{}]},{},["../node_modules/parcel-bundler/src/builtins/hmr-runtime.js","main.js"], null)
-//# sourceMappingURL=/main.1f19ae8e.js.map
+},{}]},{},["../node_modules/parcel-bundler/src/builtins/hmr-runtime.js"], null)
+//# sourceMappingURL=/style.493e6e98.js.map
--- src/main.js
+++ src/main.js
@@ -1,2 +1,55 @@
 'use strict';
 
+function getToday() {
+  var today = new Date();
+  var dd = String(today.getDate()).padStart(2, '0');
+  var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+  var yyyy = today.getFullYear();
+  today = yyyy + '-' + mm + '-' + dd;
+  return today
+}
+
+function getRequest() {
+
+  var xhr= new XMLHttpRequest()
+  xhr.open('GET', `https://api.apilayer.com/exchangerates_data/timeseries?start_date=2022-09-24&end_date=2022-10-01&base=PLN`, true)
+  xhr.setRequestHeader('apikey', '')
+ 
+  xhr.onload = function() {
+ 
+    if(xhr.status === 200) {
+      window.localStorage.setItem('data', this.responseText)
+    } else {
+      console.log('Error: ' + xhr.status)
+    }
+  } 
+  xhr.send(null)
+ }
+
+function test() {
+
+var item = window.localStorage.getItem('data')
+
+for (var k in JSON.parse(item).rates[getToday()]) {
+
+  console.log(k)
+  var el = document.getElementById('rowList')
+  var node = document.createElement('button')
+  var valnode = document.createTextNode(' ' + JSON.parse(item).rates[getToday()][k])
+  var textnode = document.createTextNode(k)
+  node.appendChild(textnode)
+  node.appendChild(valnode)
+  el.appendChild(node)
+  console.log(valnode)
+}
+console.log(getToday())
+}
+
+if (localStorage.data === undefined) {
+  getRequest()
+  test()
+} else {
+  console.log('data w localstorage')
+  test()
+}
+ 
